74cac87e29cf0a850ecce77e4df1be38cd1c16a6
acdmAnderson <acdm.anderson@hotmail.com>
2024-11-16 10:31:15 -0300

    test: ensure CreatePaymentUseCase returns 500 when PaymentIntegration throws

--- test/usecases/CreatePaymentUseCase.spec.ts
+++ test/usecases/CreatePaymentUseCase.spec.ts
@@ -2,7 +2,7 @@ import Payment from "../../src/entities/Payment"
 import PaymentStatus from "../../src/entities/PaymentStatus"
 import { IPaymentGateway } from "../../src/interfaces/IPaymentGateway"
 import { IPaymentIntegrationGateway } from "../../src/interfaces/IPaymentIntegrationGateway";
-import { ok } from "../../src/presenters/HttpResponses";
+import { internalServerError, ok } from "../../src/presenters/HttpResponses";
 import { CreatePaymentUseCase } from "../../src/usecases/CreatePaymentUseCase";
 
 const mockPaymentStatus: PaymentStatus[] = [
@@ -33,5 +33,18 @@ describe('Create Payment Use Case', () => {
         const sut = new CreatePaymentUseCase(mockPaymentGateway, mockPaymentIntegrationGateway)
         const result = await sut.execute(createPaymentCommand)
         expect(result).toEqual(ok({ paymentId: 'payment_id' }))
+    })
+    it('Should return InternalServerError when integration throws', async () => {
+        const mockPaymentINtegrationGatewayError: IPaymentIntegrationGateway = {
+            createPayment: jest.fn().mockRejectedValueOnce(new Error('Integration error'))
+        }
+        const createPaymentCommand = {
+            orderId: '1',
+            orderValue,
+            cpf: '12559757690'
+        }
+        const sut = new CreatePaymentUseCase(mockPaymentGateway, mockPaymentINtegrationGatewayError)
+        const result = await sut.execute(createPaymentCommand)
+        expect(result).toEqual(internalServerError('Error while creating a payment integration', new Error('Integration error')))
     })
 })
